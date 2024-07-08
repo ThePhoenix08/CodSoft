142ffa6f34ee8dc259bfb27b5f5d6a8e69792558
Vighnesh Brahme <v8ganesh@gmail.com>
2024-07-08 22:26:42 +0530

    add: 404 page not found page

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Hero from "./pages/Hero.tsx";
 import About from "./pages/About.tsx";
@@ -7,25 +7,42 @@ import Projects from "./pages/Projects.tsx";
 import Skills from "./pages/Skills.tsx";
 import Contact from "./pages/Contact.tsx";
 import Navbar from "./components/Navbar.tsx";
+import PageNotFound from "./pages/PageNotFound.tsx";
 
 const App: React.FC = () => {
+  const location = useLocation();
+  const validRoutes = ["/", "/about", "/projects", "/skills", "/contact"];
+  const isValidRoute = validRoutes.includes(location.pathname);
+
   return (
     <div className="app w-screen h-screen flex flex-col font-[Inter]">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Navbar />
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {isValidRoute && <Navbar />}
     </div>
   );
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ContextProvider } from "./context/Context.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ContextProvider>
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   </ContextProvider>
 );
+
 
